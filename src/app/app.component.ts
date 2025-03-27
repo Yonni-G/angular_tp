21ee98a28e62012caa41c2605e3528f071dce3a0
Yonni GUERRAULT <yonni4@gmail.com>
2025-03-27 14:53:37 +0100

    debut structure avec playlist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./pages/partials/header/header.component";
 import { FooterComponent } from "./pages/partials/footer/footer.component";
+import { PlaylistComponent } from "./pages/youtube/playlist/playlist.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, PlaylistComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
